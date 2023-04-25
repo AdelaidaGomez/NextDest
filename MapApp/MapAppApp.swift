88237464ae31b4d1cd3b7bdb98771b43b308dff7
Adelaida Gomez <agomezvieco@gmail.com>
2023-04-25 12:29:54 -0500

    Initial Commit

---
+++ MapApp/MapAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MapAppApp.swift
+//  MapApp
+//
+//  Created by Adelaida Gomez Vieco on 25/04/23.
+//
+
+import SwiftUI
+
+@main
+struct MapAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
